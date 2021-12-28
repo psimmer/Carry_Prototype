6d732e9ad84b2cda519ed5dc36adc8d8c6a08d4f
Lukas Prinz <prinz.lukas99@gmail.com>
2021-12-28 22:55:09 +0100

    Removed some cs.files Added GameManager.cs Added PatientScript.cs Added ItemType.cs

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private GameObject PopUpPrefab;
+    //All Patients Objects we have in the Prototyp
+    [SerializeField] private GameObject patientObject01;
+    [SerializeField] private GameObject patientObject02;
+    [SerializeField] private GameObject patientObject03;
+    [SerializeField] private GameObject patientObject04;
+
+    //All Patients we have in the Prototyp
+    private PatientScript patient01 = new PatientScript(100, "Patient01");
+    private PatientScript patient02 = new PatientScript(90, "Patient02");
+    private PatientScript patient03 = new PatientScript(70, "Patient03");
+    private PatientScript patient04 = new PatientScript(70, "Patient04");
+
+    private bool timer = false;
+    // 2 Lists of the Objects and the reference
+    private List<GameObject> patObjects = new List<GameObject>(4);
+    private List<PatientScript> patients = new List<PatientScript>(4);
+    void Start()
+    {
+        // Adding in Lists
+        patients.Add(patient01);
+        patients.Add(patient02);
+        patients.Add(patient03);
+        patients.Add(patient04);
+        patObjects.Add(patientObject01);
+        patObjects.Add(patientObject02);
+        patObjects.Add(patientObject03);
+        patObjects.Add(patientObject04);
+        
+    }
+
+    void Update()
+    {
+        RandomPopUp();
+    }
+    // Instantiate randomly a pop Up at a random patient
+    public void RandomPopUp()
+    {
+        int randomIndex = Random.Range(0, patients.Count);
+        PatientScript randomPatient = patients[randomIndex];
+        if (randomPatient.needSomething == false)
+        {
+            StartCoroutine("InstantiatePopUp");
+            if (timer)
+            {
+                timer = false;
+                randomPatient.needSomething = true;
+                Vector3 objectPos = patObjects[randomIndex].transform.position;
+                PopUpPrefab.transform.position = new Vector3(objectPos.x, objectPos.y + 1.5f, objectPos.z);
+                Instantiate(PopUpPrefab);
+                Debug.Log("Hello " + randomPatient.Name);
+            }
+
+
+        }
+    }
+    IEnumerator InstantiatePopUp()
+    {
+        yield return new WaitForSeconds(patient01.GetRandomTime());
+        timer = true;
+        StopCoroutine("InstantiatePopUp");
+    }
+
+   
+
+
+
+}
--- Assets/Scripts/PatientScript.cs
+++ Assets/Scripts/PatientScript.cs
@@ -2,69 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PatientScript : MonoBehaviour
+public class PatientScript
 {
-    [SerializeField] private PopUpScript popUp;        
-    [SerializeField] private GameObject popUpPrefab;    
-    [SerializeField] private int minTimeTillTask = 1;
-    [SerializeField] private int maxTimeTillTask = 5;
+    public string Name { get; set; }
+    public float patientHealth { get; set; }
+    public bool needSomething { get; set; }
+    public float randomTime { get; set; }
 
-    // This boolean goes to PopUpScript
-    private static bool recoverBool;
-    public bool RecoverBool
+    public PatientScript(float health, string name)
     {
-        get { return recoverBool; }
-        set { recoverBool = value; }
+        Name = name;
+        patientHealth = health;
     }
 
-    private void Start()
+     public float GetRandomTime()
     {
-        recoverBool = false;
-    }
-    
-
-    void Update()
-    {
-
-        if (!popUp.PopUpBool)
-        {
-            StartCoroutine("TaskPopping");
-        }
-    }
-
-    //Random number of waiting Time till Task Pops Up
-    public float RandomPopUpRange()
-    {
-        return Random.Range(minTimeTillTask, maxTimeTillTask);
-    }
-
-    //Coroutine where and when the Pop up Instantiate
-    IEnumerator TaskPopping()
-    {
-        yield return new WaitForSeconds(RandomPopUpRange());
-        popUpPrefab.transform.position = new Vector3(transform.position.x, transform.position.y + 1.5f, transform.position.z);
-        Instantiate(popUpPrefab);
-        popUp.PopUpBool = true;
-        StopCoroutine("TaskPopping");
-
-    }
-
-
-    // Controll if the nurse triggers with the Patient
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.CompareTag("Player"))
-        {
-            recoverBool = true;
-        }
-    }
-    // Controll if the nurse doesnt triggers with the Patient
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.gameObject.CompareTag("Player"))
-        {
-            recoverBool = false;
-        }
+        randomTime = Random.Range(10, 20);
+        return randomTime;
     }
 
 
