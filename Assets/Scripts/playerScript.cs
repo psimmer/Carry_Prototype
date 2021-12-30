20578f4545861b53b141df3c9bd3ba83549818f4
Lukas Prinz <prinz.lukas99@gmail.com>
2021-12-30 21:42:31 +0100

    Next try to rename

--- Assets/Scripts/playerScript.cs
+++ Assets/Scripts/playerScript.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerScript : MonoBehaviour
+public class playerScript : MonoBehaviour
 {
     public InventoryObject inventory;
 
