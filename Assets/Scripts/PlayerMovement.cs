b2c80df89c4929337437942e6ffec25decec2e2b
Alejandro <alejandro.rodriguez95@gmail.com>
2022-01-02 18:40:53 +0100

    New movement system, now uses physics and it feels almost the same as the last one. The problem with the walls has been solved!

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,13 +5,20 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float playerMovementSpeed;
+    [SerializeField] private float maxSpeed;
 
     private float horizontalMovement;
     private float verticalMovement;
+    private float verticalRotation;
+    private float horizontalRotation;
+    Rigidbody playerRigidbody;
     Transform playerTransform;
+    Vector3 maxSpeedVector;
     void Start()
     {
+        playerRigidbody = GetComponent<Rigidbody>();
         playerTransform = gameObject.transform;
+        maxSpeedVector = new Vector3(maxSpeed, 0f, maxSpeed);
     }
     void FixedUpdate()
     {
@@ -21,16 +28,48 @@ public class PlayerMovement : MonoBehaviour
 
     void playerMovement()
     {
-        horizontalMovement = Input.GetAxis("Horizontal");
-        verticalMovement = Input.GetAxis("Vertical");
+        horizontalRotation = Input.GetAxis("Horizontal");
+        verticalRotation = Input.GetAxis("Vertical");
+        horizontalMovement = 0;
+        verticalMovement = 0;
 
-        Vector3 movement = new Vector3(horizontalMovement, 0.0f, verticalMovement);
+        if (Input.GetKey(KeyCode.W))
+        {
+            verticalMovement = 1f;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            verticalMovement = -1f;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            horizontalMovement = -1f;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            horizontalMovement = 1f;
+        }
+
+
+
+        Vector3 movement = new Vector3(horizontalMovement, 0f, verticalMovement);
+        Vector3 rotation = new Vector3(horizontalRotation, 0f, verticalRotation);
+        movement = movement.normalized * Time.deltaTime * playerMovementSpeed;
+        Debug.Log(movement);
+
+
+        // if the axis input is higher than 0.17 (number pulled out of my ass, but it works nicely)
+        // then rotate player in the direction of the input. Without this number, it will start rotating constantly.
         if (Mathf.Abs(horizontalMovement) > 0.17 || Mathf.Abs(verticalMovement) > 0.17)
         {
-            playerTransform.rotation = Quaternion.LookRotation(movement);
+            playerTransform.rotation = Quaternion.LookRotation(rotation);
         }
-        playerTransform.Translate(movement * playerMovementSpeed * Time.deltaTime, Space.World);
 
+        // check if current velocity is lower than max speed, if so, you can keep adding force 
+        if (playerRigidbody.velocity.magnitude < maxSpeed)
+        {
+            playerRigidbody.AddForce(movement);
+        }
 
     }
 }
