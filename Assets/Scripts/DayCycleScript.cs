f799395512b1b3a8619cfce8792cc1765f358282
Lukas Prinz <prinz.lukas99@gmail.com>
2022-01-02 19:10:04 +0100

    Added DayCycleScript.cs Added some SpotLights

---
+++ Assets/Scripts/DayCycleScript.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayCycleScript : MonoBehaviour
+{
+    [SerializeField] private float start = 1f;
+    [SerializeField] private float end = 180f;
+
+    private Quaternion dayStart;
+    private Quaternion dayEnd;
+    float speed = 0.1f;
+    // The speed is calculatet to 6 minutes till the lerp finish
+    void Start()
+    {
+        dayStart = Quaternion.Euler(start, 90, 0);
+        dayEnd = Quaternion.Euler(end, 90, 0);
+    }
+
+    void Update()
+    {
+        transform.rotation = Quaternion.Lerp(dayStart, dayEnd, Time.time * (speed / (6 * 6)));
+        Debug.Log((int)Time.time);
+    }
+}
