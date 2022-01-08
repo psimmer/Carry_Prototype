5c0d38e35419d646aecdeadd8225e7e9a1be9f45
Lukas Prinz <prinz.lukas99@gmail.com>
2022-01-08 22:24:00 +0100

    tried to solve the DayCycleScript issue but it isnt solved

--- Assets/Scripts/DayCycleScript.cs
+++ Assets/Scripts/DayCycleScript.cs
@@ -10,6 +10,7 @@ public class DayCycleScript : MonoBehaviour
     private Quaternion dayStart;
     private Quaternion dayEnd;
     float speed = 0.1f;
+
     // The speed is calculatet to 6 minutes till the lerp finish
     void Start()
     {
@@ -19,7 +20,16 @@ public class DayCycleScript : MonoBehaviour
 
     void Update()
     {
+        LerpStart();
+    }
+
+
+    public void LerpStart()
+    {
         transform.rotation = Quaternion.Lerp(dayStart, dayEnd, Time.time * (speed / (6 * 6)));
         //Debug.Log((int)Time.time);
+        Debug.Log("Hello i am the sun and i dont know why i am still rotating in the menu scene");
+        //Debug.Log(transform.rotation);
     }
+
 }
